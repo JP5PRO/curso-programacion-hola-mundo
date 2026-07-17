fcf719abe0477a8f647ba084789fb15aa2a6cb81
JP5PRO <jp5200908@example.com>
2026-07-16 18:34:45 -0600

    Evidencias de Actividad

--- Programas_Basicos/avg_num.py
+++ Programas_Basicos/avg_num.py
@@ -5,4 +5,4 @@ num2 = float(input("Ingresa otro número: "))
 num3 = float(input("Ingresa tu último número: "))
 
 avg = ((num1 + num2 + num3) / 3)
-print(f"El promedio de tus tres números es de: {avg}")
+print(f"El promedio de tus tres números {num1}, {num2} y {num3} es de: {avg}")
